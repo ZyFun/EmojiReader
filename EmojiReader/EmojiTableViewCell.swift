c37b2d3c788da816aa92a9f196731eb3d94997db
Dmitriy Danilin <danilindn@icloud.com>
2020-06-09 15:02:15 +0300

    Окончание урока

---
+++ EmojiReader/EmojiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by Дмитрий Данилин on 09.06.2020.
+//  Copyright © 2020 Дмитрий Данилин. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
