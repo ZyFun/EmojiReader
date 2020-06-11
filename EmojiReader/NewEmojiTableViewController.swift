fea97c9f95324671f0a7f1833f86371ab3f5ab6f
Dmitriy Danilin <danilindn@icloud.com>
2020-06-11 16:24:55 +0300

    Закончил 9 урок.

--- EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
     
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavorit: false)
+    
     @IBOutlet weak var emojiTextF: UITextField!
     @IBOutlet weak var nameTextF: UITextField!
     @IBOutlet weak var descriptionTextF: UITextField!
@@ -18,14 +20,28 @@ class NewEmojiTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        updateSaveButton()
+    }
+    
+    private func updateSaveButton() {
+        let emojiText = emojiTextF.text ?? ""
+        let nameText = nameTextF.text ?? ""
+        let descriptionText = descriptionTextF.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
     }
     
     @IBAction func textChanged(_ sender: UITextField) {
+        updateSaveButton()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "save" else { return }
+        let emoji = emojiTextF.text ?? ""
+        let name = nameTextF.text ?? ""
+        let description = descriptionTextF.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavorit: self.emoji.isFavorit)
     }
 }
